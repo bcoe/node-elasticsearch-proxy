b1f1589a62f819ff82ab232d09cb90c9da1b62db
bcoe <bencoe@gmail.com>
2011-08-22 18:07:17 -0700

    Fixed a few bugs with how ESProxy handles elasticsearch returning failures.

--- example.js
+++ example.js
@@ -1,9 +1,8 @@
-var ESProxy = require('./lib/elasticsearch-proxy').ESProxy,
+var ESProxy = require('./lib').ESProxy,
 	sys = require('sys');
 
-sys.puts("Routing 9200 to 9210")
+sys.puts("Routing 9201 to 9200")
 new ESProxy({
-	'proxyPort': 9200,
-	'elasticSearchPort': 9210,
-	'maximumOpenIndices': 5
+	'proxyPort': 9201,
+	'elasticSearchPort': 9200
 });
